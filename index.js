2bb3a39f4fc1ba69f0328a074412110d0615f2e1
DidDog <94751052+Did-Dog@users.noreply.github.com>
2021-12-31 15:02:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ function game_block_it() {
 function game_bubble_shooter() {
   location.href = "bubble_shooter";
 }
+function game_canvas() {
+  location.href = "canvas";
+}
+function game_match_3() {
+  location.href = "match_3";
+}
+function game_snake() {
+  location.href = "snake";
+}
